7741320ffed56e5a1734d3519d72535e56e5678f
Shiro219 <rumendez@csumb.edu>
2025-01-30 15:18:53 -0800

    Login page fixed

--- app/(tabs)/Signin.tsx
+++ app/(tabs)/Signin.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./sign.css";
 
+
+
+
 function sign() {
     return (
         <div className="Login">
-            <div className="Login-container">
+            <div className="LoginBox">
                 <div className="LoginHeader"> Login </div>
                 <div className="inputs">
                     <input type="Email" placeholder="Enter your Email" />
